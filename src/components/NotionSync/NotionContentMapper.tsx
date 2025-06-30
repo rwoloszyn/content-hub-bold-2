dad461e8bd2d8786f6deb5f8155a42b5ce2f2d42
Rafal Woloszyn <rwoloszyn@pl.imshealth.com>
2025-06-30 18:41:54 +0200

    fix/netfli_build

---
+++ src/components/NotionSync/NotionContentMapper.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface NotionContentMapperProps {
+  // Add your props here
+}
+
+export const NotionContentMapper: React.FC<NotionContentMapperProps> = () => {
+  return (
+    <div className="p-4">
+      <h3 className="text-lg font-semibold mb-4">Content Mapper</h3>
+      <p className="text-gray-600">Notion content mapping functionality will be implemented here.</p>
+    </div>
+  );
+}; 
